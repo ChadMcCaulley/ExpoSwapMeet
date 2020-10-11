f60f074d7de918543e3b2fe6702b205d310125c9
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-10-11 16:03:19 -0700

    Learning firestore and expo development using chat app

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
-const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain:  `${process.env.FIREBASE_PROJECT_ID}.firebaseapp.com`,
-  databaseURL: "https://project-id.firebaseio.com",
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: `${process.env.FIREBASE_PROJECT_ID}.appspot.com`,
-  messagingSenderId: "sender-id",
-  appId: "app-id",
-  measurementId: "G-measurement-id"
-}; 
+export default {
+  apiKey: "AIzaSyAtMh9AVKbV9eSl9n3uMA9IhkyDktDjwBw",
+  authDomain: "swapmeet-4848e.firebaseapp.com",
+  databaseURL: "https://swapmeet-4848e.firebaseio.com",
+  projectId: "swapmeet-4848e",
+  storageBucket: "swapmeet-4848e.appspot.com",
+  messagingSenderId: "846829359468",
+  appId: "1:846829359468:web:472b5631eac2948ad22349",
+  measurementId: "G-32LDMXMXEY"
+}
