7626d42e1d879638e170250fa4ce9e7872d967cb
Chad McCaulley <chad.mccaulley@pgm-corp.com>
2020-10-12 19:52:49 -0700

    Trying to get envs to work app wide

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import Expo from 'expo'
+import React from 'react'
 import { StyleSheet, View, Text, Button  } from 'react-native'
 import AppStyles from '../AppStyles'
 
+
 export default function LoginScreen({ navigation }) {
+  signInWithGoogle = async () => {
+    console.log('HERE')
+    console.log(process.env)
+    try {
+      const result = await Expo.Google.logInAsync({
+        behavior: 'web',
+        androidClientId: process.env.GOOGLE_AUTH_ANDROID_ID,
+        iosClientId: process.env.GOOGLE_AUTH_IOS_ID,
+        scopes: ['profile', 'email']
+      })
+      if (result.type === 'success') return result.accessToken
+      return { cancelled: true }
+    } catch (err) {
+      return { error: true }
+    }
+  }
 
   return (
     <View style={AppStyles.container}>
-      <Button title="Google" />
+      <Button title="Google" onPress={signInWithGoogle}/>
     </View>
   )
 }
